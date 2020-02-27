772e41b6250005392378d0ab6eec9c0c08217718
Joaquin P <joaquinpb.tech@gmail.com>
2020-02-27 12:48:28 +0100

    Initialized start.spring.io with Spring web, Spring Security, Spring Data JPA, and driver postgrSQL

---
+++ src/test/java/com/jpbtech/webappservice/UserManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpbtech.webappservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
