7f6105d8dca07657f102926c3ea51390e70bd951
Joaquin P <joaquinpb.tech@gmail.com>
2020-03-03 11:44:50 +0100

    version 1 - security no estable

--- src/main/java/com/jpbtech/webappservice/exceptions/ExceptionInDataBase.java
+++ src/main/java/com/jpbtech/webappservice/exceptions/ExceptionInDataBase.java
@@ -1,11 +1,8 @@
 package com.jpbtech.webappservice.exceptions;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,7 +12,6 @@ public class ExceptionInDataBase extends Exception {
 
 	
 	private static final long serialVersionUID = 1L;
-	private String fieldDetail;
 	private Date timestamp;
 	private ArrayList<String> details;
 	
--- src/main/java/com/jpbtech/webappservice/service/UserServiceImpl.java
+++ src/main/java/com/jpbtech/webappservice/service/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package com.jpbtech.webappservice.service;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
